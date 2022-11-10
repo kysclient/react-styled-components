0ef69e3a8f85edd194915f9af11f70075c1edc57
91308227 <91308227@ktfriend.com>
2022-11-10 14:02:02 +0900

    styled-components basic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled, {keyframes} from "styled-components";
 
 function App() {
-  return null;
+
+    const Wrapper = styled.div`
+      display: flex;
+      height: 100vh;
+      width: 100vw;
+      justify-content: center;
+      align-items: center;
+      background-color: ${(props) => props.theme.backgroundColor}
+    `
+    const Title = styled.h1`
+      color: ${props => props.theme.textColor};
+    `
+
+    return (
+        <Wrapper>
+                <Title>hi</Title>
+        </Wrapper>
+    );
 }
 
 export default App;
